576b79619ea36fffcc641653914e1bda9ffec91f
OjaanIr <farnasrozaan7@gmail.com>
2023-11-29 15:14:22 +0700

    feat: payment form

--- payment-app/payment_process/urls.py
+++ payment-app/payment_process/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import process_payment
+from .views import process_payment, payment_form
 
 urlpatterns = [
     path('process-payment/', process_payment, name='process_payment'),
+    path('payment-form/', payment_form, name='payment_form'),
 ]
--- payment-app/payment_process/views.py
+++ payment-app/payment_process/views.py
@@ -1,16 +1,26 @@
 import json
 import random
 import requests
+import asyncio
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.urls import reverse
 from .models import Invoice
+from django.shortcuts import render
 
 @csrf_exempt
-def process_payment(request):
+def send_invoice_id(request):
+    while True:
+        invoice_id = ''.join(random.choices('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', k=8))
+        if not Invoice.objects.filter(invoice_id=invoice_id).exists():
+            return invoice_id, request.build_absolute_uri(reverse('payment_form')) + f'?invoice_id={invoice_id}'
+
+@csrf_exempt
+async def process_payment(request):
     invoice_id = request.POST.get('invoice_id')
 
     # Simulasi kegagalan 10%
-    status = random.choices([True, False], weights=[90, 10])[0]
+    status = await random.choices([True, False], weights=[90, 10])[0]
 
     invoice = Invoice.objects.create(
         invoice_id=invoice_id,
@@ -23,6 +33,12 @@ def process_payment(request):
         'invoice_id': invoice.invoice_id,
         'status': invoice.status,
     }
-    requests.post(webhook_url, data=json.dumps(payload), headers={'Content-Type': 'application/json'})
+
+    loop = asyncio.get_event_loop()
+    await loop.run_in_executor(None, lambda: requests.post(webhook_url, data=json.dumps(payload), headers={'Content-Type': 'application/json'}))
     
     return JsonResponse({'message': 'Payment process done!'})
+
+def payment_form(request):
+    invoice_id = request.GET.get('invoice_id')
+    return render(request, 'payment_form.html', {'invoice_id': invoice_id})
